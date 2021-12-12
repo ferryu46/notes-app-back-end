92a46b8ead2c9f2e657098b7aece81425e0f179b
ferryu46 <ferry.yunkurniawan@dikbud.belajar.id>
2021-12-13 00:19:27 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.44.174',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
